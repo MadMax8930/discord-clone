7d95a7a87d6e184482c1ca8f90ff11ba68b8dc4c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-07 01:56:10 +0200

    channel dropdown

--- src/components/ChannelSection.jsx
+++ src/components/ChannelSection.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useState } from 'react';
+import { BsHash } from 'react-icons/bs';
+import { FaChevronDown, FaChevronRight, FaPlus } from 'react-icons/fa';
 
-function ChannelSection() {
+const topics = ['tailwind-css', 'react'];
+const questions = ['jit-compilation', 'purge-files', 'dark-mode'];
+const random = ['variants', 'plugins'];
+
+const ChannelSection = () => {
   return (
-    <div>ChannelSection</div>
-  )
+    <div className='channel-section'>
+      <div className='channel-content'>
+         <Dropdown header="Topics" selections={topics}/>
+         <Dropdown header="Questions" selections={questions}/>
+         <Dropdown header="Random" selections={random}/>
+      </div>
+
+    </div>
+  );
+};
+
+const Dropdown = ({ header, selections }) => {
+   const [expanded, setExpanded] = useState(true);
+
+   return (
+      <div className='dropdown'>
+         <div onClick={() => setExpanded(!expanded)}>
+           <ChevronIcon expanded={expanded} />
+           <h5 className={expanded ? 'dropdown-header-text-selected' : 'dropdown-header-text'}>{header}</h5>
+           <FaPlus size='12' className='text-accent text-opacity-80 my-auto ml-auto' />
+         </div>
+         {expanded && selections 
+                   && selections.map((selection) =>(<TopicSelection selection={selection} />))}
+      </div>
+   )
 }
+
+const ChevronIcon = ({ expanded }) => {
+   const ChevClass = 'text-accent text-opacity-80 my-auto mr-1';
+   return expanded 
+   ? 
+   (<FaChevronDown size='14' className={ChevClass} />) 
+   :
+   (<FaChevronRight size='14' className={ChevClass} />)
+}
+
+const TopicSelection = ({ selection }) => {
+   return (
+      <div className='dropdown-selection'>
+         <BsHash size='24' className='text-gray-400' />
+         <h5 className='dropdown-selection-text'>{selection}</h5>
+      </div>
+   )
+}
+
 
 export default ChannelSection
