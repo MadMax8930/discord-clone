0fa9364986a5390c39d622a09b7c6a8a5bbaec52
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-07 03:40:58 +0200

    topbar search

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -11,20 +11,26 @@ import {
 const TopBar = () => {
   return (
     <div className='top-nav'>
-       {/* <HashtagIcon />
+       <HashIcon />
        <Title />
-       <ThemeIcon />
+       {/* <ThemeIcon /> */}
        <Search />
        <BellIcon />
-       <UserCircle /> */}
-       <li>a</li>
-       <li>a</li>
-       <li>a</li>
-       <li>a</li>
-       <li>a</li>
-       <li>a</li>
+       <UserIcon />
     </div>
   )
 }
+
+const Search = () => (
+   <div className='top-search'>
+     <input className='top-search-input' type='text' placeholder='Search...' />
+     <FaSearch size='18' className='text-secondary my-auto' />
+   </div>
+);
+
+const BellIcon = () => (<FaRegBell size='24' className='top-icon' />)
+const UserIcon = () => (<FaUserCircle size='24' className='top-icon' />)
+const HashIcon = () => (<FaHashtag size='20' className='top-hash'/>)
+const Title = () => (<h5 className='top-title'>tailwind-css</h5>)
 
 export default TopBar
